6ab5c1bbbefb7bd9d2c1b24010eb3deac5835880
Bob <99611400tc@gmail.com>
2020-09-22 22:43:20 +0800

    添加了.gitignore文件

---
+++ independentHomeworkMatrixMultiply/parallel/cmake-build-debug/CMakeFiles/0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0.exe"
+  "0.exe.manifest"
+  "0.lib"
+  "0.pdb"
+  "CMakeFiles/0.dir/0_generated_0.cu.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
