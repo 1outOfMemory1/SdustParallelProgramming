1375907f7d1582923404aef37c568216b8995705
Bob <99611400tc@gmail.com>
2020-10-20 20:14:02 +0800

    从linux的clion 修改了一下子  可能有点问题

---
+++ cuda/independentHomeworkMatrixMultiply/parallel/cmake-build-debug/CMakeFiles/knn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/knn.dir/knn_generated_knn.cu.o"
+  "knn"
+  "knn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/knn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cuda/independentHomeworkMatrixMultiply/parallel/cmake-build-debug/CMakeFiles/yhn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yhn.dir/yhn_generated_yhn.cu.o"
+  "yhn"
+  "yhn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/yhn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
